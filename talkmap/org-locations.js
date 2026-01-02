9a0c475313752e3b0021591e06413f5cdade8959
Matthias Niehoff <matthias.niehoff@codecentric.de>
2026-01-02 11:18:25 +0100

    update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,16 +1,31 @@
 var addressPoints = [
   [
+    "Can Data & ML really learn from software engineering?<br />voxxed days Brussels 2024; Brussels, Belgium",
+    50.8465573,
+    4.351697
+  ],
+  [
     "Datenarchitekturen in der Realit\u00e4t<br />Software Architecture Alliance 2024; Munich, Germany",
     48.1371079,
     11.5753822
   ],
   [
-    "Data Aware Architectures als Basis f\u00fcr KI & Analytics<br />techcamp 2025; Hamburg, Germany",
-    53.550341,
-    10.000654
+    "Mit duckdb schell und einfach Daten analysieren \u2013 egal, wo sie gespeichert sind<br />data2day 2025; Karlsruhe, Germany",
+    49.0068705,
+    8.4034195
+  ],
+  [
+    "Modern Data Architectures need Software Engineering<br />WeAreDevelopers World Congress 2024; Berlin, Germany",
+    52.5173885,
+    13.3951309
   ],
   [
     "Modern Data Architectures - What They Are and How to Implement?<br />iSAQB Software Architecture Gathering \u2013 Digital 2023; Online",
+    43.5904719,
+    3.8595132
+  ],
+  [
+    "Continuous Integration for ML applications - a practical example<br />WeAreDevelopers Live 2022; Online",
     43.5904719,
     3.8595132
   ],
@@ -20,54 +35,9 @@ var addressPoints = [
     8.694724
   ],
   [
-    "Data pipelines, Documentation and Lineage with SQL & dbt<br />NDC Porto 2025; Porto, Portugal",
-    41.1502195,
-    -8.6103497
-  ],
-  [
-    "Data architectures in the real world<br />iSAQB Software Architecture Gathering 2024; Berlin, Germany",
-    52.5173885,
-    13.3951309
-  ],
-  [
-    "Can Data & ML really learn from software engineering?<br />\u00d8redev 2024; Malm\u00f6, Sweden",
-    55.6052931,
-    13.0001566
-  ],
-  [
-    "Data Aware Architectures als Basis f\u00fcr KI & Analytics<br />SQL Konferenz 2025; Hanau am Main, Germany",
-    50.1217213,
-    8.9085545
-  ],
-  [
-    "Data-Aware Architectures als Basis f\u00fcr Analytics & AI<br />OOP 2025; Munich, Germany",
-    48.1371079,
-    11.5753822
-  ],
-  [
-    "Simplicity in Data & ML<br />WeAreDevelopers World Congress 2024; Berlin, Germany",
-    52.5173885,
-    13.3951309
-  ],
-  [
     "Mit duckdb schell und einfach Daten analysieren - egal wo sie gespeichert sind<br />m3 Konferenz 2025; Karlsruhe, Germany",
     49.0068705,
     8.4034195
-  ],
-  [
-    "Moderne Datenarchitekturen: Was sind sie und wie werden sie umgesetzt?<br />Developer Week 2024; N\u00fcrnberg, Germany",
-    49.453872,
-    11.077298
-  ],
-  [
-    "Continuous Integration for ML applications - a practical example<br />WeAreDevelopers Live 2022; Online",
-    43.5904719,
-    3.8595132
-  ],
-  [
-    "Data Aware Architectures als Basis f\u00fcr KI & Analytics<br />Developer Week 2025; Mannheim, Germany",
-    49.4892913,
-    8.4673098
   ],
   [
     "Analyze data easily with duckdb - and the implications on data architectures<br />pycon/pydata DE 2025; Darmstadt, Germany",
@@ -75,19 +45,24 @@ var addressPoints = [
     8.651177
   ],
   [
+    "Mit duckdb schell und einfach Daten analysieren \u2013 egal, wo sie gespeichert sind<br />IT Tage 2025; Frankfurt, Germany",
+    50.1106444,
+    8.6820917
+  ],
+  [
+    "Data Aware Architectures als Basis f\u00fcr KI & Analytics<br />SQL Konferenz 2025; Hanau am Main, Germany",
+    50.1217213,
+    8.9085545
+  ],
+  [
     "Reliable Data Flows and Scalable Platforms: Tackling Key Data Challenges<br />qcon 2025; London, United Kingdom",
     51.5074456,
     -0.1277653
   ],
   [
-    "Modern Data Architectures need Software Engineering<br />WeAreDevelopers World Congress 2024; Berlin, Germany",
-    52.5173885,
-    13.3951309
-  ],
-  [
-    "Can Data & ML really learn from software engineering?<br />voxxed days Brussels 2024; Brussels, Belgium",
-    50.8465573,
-    4.351697
+    "Data-Aware Architectures als Basis f\u00fcr Analytics & AI<br />OOP 2025; Munich, Germany",
+    48.1371079,
+    11.5753822
   ],
   [
     "K\u00f6nnen wir wirklich vom Software Engineering lernen?<br />m3 Konferenz 2024; K\u00f6ln, Germany",
@@ -95,8 +70,63 @@ var addressPoints = [
     6.959974
   ],
   [
+    "DuckDB als schnelle, einfache und g\u00fcnstige SQL Engine<br />TDWI 2025; M\u00fcnchen, Germany",
+    48.1371079,
+    11.5753822
+  ],
+  [
     "Enjoying SQL data pipelines with dbt<br />WeAreDevelopers World Congress 2023; Berlin, Germany",
     52.5173885,
     13.3951309
+  ],
+  [
+    "Moderne Datenarchitekturen: Was sind sie und wie werden sie umgesetzt?<br />Developer Week 2024; N\u00fcrnberg, Germany",
+    49.453872,
+    11.077298
+  ],
+  [
+    "Can Data & ML really learn from software engineering?<br />\u00d8redev 2024; Malm\u00f6, Sweden",
+    55.6052931,
+    13.0001566
+  ],
+  [
+    "Data architectures in the real world<br />iSAQB Software Architecture Gathering 2024; Berlin, Germany",
+    52.5173885,
+    13.3951309
+  ],
+  [
+    "Data pipelines, Documentation and Lineage with SQL & dbt<br />NDC Porto 2025; Porto, Portugal",
+    41.1502195,
+    -8.6103497
+  ],
+  [
+    "Data Aware Architectures als Basis f\u00fcr KI & Analytics<br />Developer Week 2025; Mannheim, Germany",
+    49.4892913,
+    8.4673098
+  ],
+  [
+    "Datenanalysen zur Versorgungssicherheit \u2013 Eine Plattform f\u00fcr Fachleute und Data Engineers<br />data2day 2025; Karlsruhe, Germany",
+    49.0068705,
+    8.4034195
+  ],
+  [
+    "Data Aware Architectures als Basis f\u00fcr KI & Analytics<br />techcamp 2025; Hamburg, Germany",
+    53.550341,
+    10.000654
+  ],
+  [
+    "Simplicity in Data & ML<br />WeAreDevelopers World Congress 2024; Berlin, Germany",
+    52.5173885,
+    13.3951309
+  ],
+  [
+    "Datenarchitekturen in der Realit\u00e4t<br />TDWI 2025; M\u00fcnchen, Germany",
+    48.1371079,
+    11.5753822
+  ],
+  [
+    "Mit duckdb schell und einfach Daten analysieren - egal wo sie gespeichert sind<br />JavaLand 2025; N\u00fcrburg, Germany",
+    50.3413391,
+    6.9519229
   ]
 ];
